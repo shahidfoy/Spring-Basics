9ac282eac1b639ba9c1f67433de746c1067dff9b
Shahid Foy <shahidfoy@gmail.com>
2018-04-24 00:13:01 -0600

    Update SpringInSimpleStepsApplication.java

--- src/main/java/com/shaidfoy/spring/basics/springinsimplesteps/SpringInSimpleStepsApplication.java
+++ src/main/java/com/shaidfoy/spring/basics/springinsimplesteps/SpringInSimpleStepsApplication.java
@@ -6,10 +6,6 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringInSimpleStepsApplication {
-	
-	// What are the beans?
-	// What are the dependencies of a bean?
-	// Where to search for beans? because we are in the same package the beans are auto searched
 
 	public static void main(String[] args) {		
 		// Application Context maintains all the beans
